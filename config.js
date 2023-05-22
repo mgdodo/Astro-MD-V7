b327fedbffd7ca85cd73a54cbfe2b480e5332ba1
mgdodo <134281370+mgdodo@users.noreply.github.com>
2023-05-22 14:48:34 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
+LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
+ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.ibb.co/p42b4pz/Fubuki-Atsuya.jpg' : process.env.ALIVE_LOGO,
+FOOTER: process.env.FOOTER === undefined ? 'ᴍɢᴅᴀᴅ ʙᴏᴛ' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ мg∂α∂ вσт*' : process.env.CAPTION,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
+INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
+HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=sjIFATKa#Hz5WOGuRAuu1oXlfYdn00eUKMUGgJkg_s-tAvvIT1MM' : process.env.SESSION_ID
+};
